24afe964716b3f938aa72ddba58df558a9520efa
leen-neel <and24903@gmail.com>
2022-10-04 10:31:36 +0530

    the dinosaurs now can be fetched from the api

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,16 +3,21 @@ dotenv.config();
 
 const API_URL = process.env.API_URL;
 
-export const getPosts = async () => {
-	let posts = {};
+export async function fetchAPI(path) {
+	const res = await fetch(`${API_URL}${path}`);
+	const json = await res.json();
 
-	fetch(`${process.env.USER}`).then((data) => {
-		data.json().then((json) => {
-			posts = json;
-		});
-	});
-	console.log(posts);
+	return json;
+}
+
+export async function getDinos() {
+	let posts = await fetchAPI('dinos?per_page=50');
 	return posts;
-};
+}
+
+export async function getDino(slug) {
+	let posts = await fetchAPI(`dinos?slug=${slug}`);
+	return posts[0];
+}
 
 export { API_URL };
