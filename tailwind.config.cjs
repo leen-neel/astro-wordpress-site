1f19b565fd94fae8081031852296072bddf345c8
leen-neel <and24903@gmail.com>
2022-10-04 11:16:53 +0530

    added hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#121212',
+			},
+		},
 	},
-	plugins: [],
-}
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#d94948',
+					secondary: '#4959ed',
+					accent: '#38077c',
+					neutral: '#1F2228',
+					'base-100': '#1f2937',
+					info: '#5eead4',
+					success: '#126E52',
+					warning: '#C49B08',
+					error: '#FA1E29',
+				},
+			},
+		],
+	},
+};
