4a3253358abd4b40df714fe141992972427be5c3
leen-neel <and24903@gmail.com>
2022-10-05 14:03:10 +0530

    added meta tags

---
+++ config.js
@@ -0,0 +1,7 @@
+export const SITE = {
+	title: 'Norian Games - Where vision and passion go hand in hand',
+	description:
+		'We are a bunch of arduous and passionate game developers striving to create the games that you will relish as much as we do. With creativity and imagination as the fulcrum in our games’ making, we apply our heart and soul in our attempts so that the player will have an experience that is unique to our games. We look forward to your support and encouragement as we take our stroll on this path together.',
+	image: 'https://norian.studio/wp-content/uploads/2019/05/PrimarySVG.svg',
+	url: 'https://norian.studio/',
+};
